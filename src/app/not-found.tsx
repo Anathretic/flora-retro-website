7a40d97b9f015a2639708288fd6864d569af57cf
Konrad <konrad.wojtylo.9@gmail.com>
2025-04-10 23:07:08 +0200

    Created not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { useEffect } from 'react';
+import Link from 'next/dist/client/link';
+import Image from 'next/image';
 import { useFooterLinksContext } from '@/hooks/useFooterLinksContext';
 import { Footer } from '@/components';
+
+import NotFoundImage from '../images/not-found-image.png';
+import styles from '../styles/not-found/styles.module.scss';
 
 export default function NotFound() {
 	const { setShowSpecialLinks } = useFooterLinksContext();
@@ -12,9 +17,22 @@ export default function NotFound() {
 	}, []);
 
 	return (
-		<div>
-			<h1>Nie znalezioino strony!</h1>
+		<>
+			<section className={styles.notfound}>
+				<div className={styles.notfound__container}>
+					<Image className={styles.notfound__img} src={NotFoundImage} alt='Obrazek informujący o braku strony' />
+					<div className={styles.notfound__box}>
+						<h1 className={styles.notfound__title}>Ups! Strony nie znaleziono..</h1>
+						<p className={styles.notfound__credits}>
+							Chyba link się nie zgadza.. Skorzystaj z przycisku poniżej i zacznij swoje poszukiwanie od głównej strony!
+						</p>
+						<Link href='/' className={styles.notfound__btn} aria-label='Powrót na stronę główną'>
+							Powrót
+						</Link>
+					</div>
+				</div>
+			</section>
 			<Footer />
-		</div>
+		</>
 	);
 }
