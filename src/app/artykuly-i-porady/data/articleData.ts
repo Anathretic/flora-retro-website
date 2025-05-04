e0765e50c8c424be170cb1e432e80dbbd0abd026
Konrad <konrad.wojtylo.9@gmail.com>
2025-05-04 02:22:58 +0200

    Created skeleton of articles array and prepare it for further work

---
+++ src/app/artykuly-i-porady/data/articleData.ts
@@ -0,0 +1,30 @@
+export const articleData = [
+	{
+		id: 0,
+		title: 'Lorem ipsum dolor sit',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt recusandae in deleniti animi nesciunt quasi molestiae eum sunt ut. Animi nesciunt quasi molestiae eum sunt ut.',
+		specialText: 'Vel quidem dicta voluptates repellat laborum exercitationem eius, nulla saepe, iste ex.',
+		href: '',
+	},
+	{
+		id: 1,
+		title: 'Lorem ipsum dolor sit',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt recusandae in deleniti animi nesciunt quasi molestiae eum sunt ut. Animi nesciunt quasi molestiae eum sunt ut.',
+		specialText: 'Vel quidem dicta voluptates repellat laborum exercitationem eius, nulla saepe, iste ex.',
+		href: '',
+	},
+	{
+		id: 2,
+		title: 'Lorem ipsum dolor sit',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt recusandae in deleniti animi nesciunt quasi molestiae eum sunt ut. Animi nesciunt quasi molestiae eum sunt ut.',
+		specialText: 'Vel quidem dicta voluptates repellat laborum exercitationem eius, nulla saepe, iste ex.',
+		href: '',
+	},
+	{
+		id: 3,
+		title: 'Lorem ipsum dolor sit',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt recusandae in deleniti animi nesciunt quasi molestiae eum sunt ut. Animi nesciunt quasi molestiae eum sunt ut.',
+		specialText: 'Vel quidem dicta voluptates repellat laborum exercitationem eius, nulla saepe, iste ex.',
+		href: '',
+	},
+];
