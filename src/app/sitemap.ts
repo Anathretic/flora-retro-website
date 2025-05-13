153f99d0d2aaccd41867ea6b7140db4ad5139338
Konrad <konrad.wojtylo.9@gmail.com>
2025-05-14 00:11:17 +0200

    Added sitemap and robots for better seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,63 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	const baseUrl = 'https://flora-retro.pl';
+	const date = new Date();
+
+	return [
+		{
+			url: `${baseUrl}/`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 1,
+		},
+		{
+			url: `${baseUrl}/artykuly-i-porady`,
+			lastModified: date,
+			changeFrequency: 'weekly',
+			priority: 0.9,
+		},
+		{
+			url: `${baseUrl}/dekoracje`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: `${baseUrl}/wynajem-samochodu`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.7,
+		},
+		{
+			url: `${baseUrl}/polityka-prywatnosci`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.3,
+		},
+		{
+			url: `${baseUrl}/artykuly-i-porady/magiczne-dekoracje-slubne`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.9,
+		},
+		{
+			url: `${baseUrl}/artykuly-i-porady/top-5-dekoracji-sali-weselnej`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.9,
+		},
+		{
+			url: `${baseUrl}/artykuly-i-porady/jak-wybrac-auto-do-slubu`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.9,
+		},
+		{
+			url: `${baseUrl}/artykuly-i-porady/dekoracje-na-przyjecia`,
+			lastModified: date,
+			changeFrequency: 'monthly',
+			priority: 0.9,
+		},
+	];
+}
