4520867426e89efb65ef65bde0668cf95083bd08
Konrad <konrad.wojtylo.9@gmail.com>
2025-05-18 01:11:43 +0200

    Added image background to offer box and made minor change to notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,46 +1,31 @@
-'use client';
-
-import { useEffect } from 'react';
 import Link from 'next/dist/client/link';
 import Image from 'next/image';
-import { useFooterLinksContext } from '@/hooks/useFooterLinksContext';
-import { Footer } from '@/components';
 
 import NotFoundImage from '../images/subpages/not-found-subpage/not-found-image.png';
 import styles from '../styles/not-found/styles.module.scss';
 
 export default function NotFound() {
-	const { setShowSpecialLinks } = useFooterLinksContext();
-
-	useEffect(() => {
-		setShowSpecialLinks(true);
-	}, []);
-
 	return (
-		<>
-			<main>
-				<section className={styles.notfound}>
-					<div className={styles.notfound__container}>
-						<Image
-							className={styles.notfound__img}
-							src={NotFoundImage}
-							alt='Obrazek informujący o braku strony'
-							priority
-						/>
-						<div className={styles.notfound__box}>
-							<h1 className={styles.notfound__title}>Ups! Strony nie znaleziono..</h1>
-							<p className={styles.notfound__credits}>
-								Chyba link się nie zgadza.. Skorzystaj z przycisku poniżej i zacznij swoje poszukiwanie od głównej
-								strony!
-							</p>
-							<Link href='/' className={styles.notfound__btn} aria-label='Powrót na stronę główną'>
-								Powrót
-							</Link>
-						</div>
+		<main>
+			<section className={styles.notfound}>
+				<div className={styles.notfound__container}>
+					<Image
+						className={styles.notfound__img}
+						src={NotFoundImage}
+						alt='Obrazek informujący o braku strony'
+						priority
+					/>
+					<div className={styles.notfound__box}>
+						<h1 className={styles.notfound__title}>Ups! Strony nie znaleziono..</h1>
+						<p className={styles.notfound__credits}>
+							Chyba link się nie zgadza.. Skorzystaj z przycisku poniżej i zacznij swoje poszukiwanie od głównej strony!
+						</p>
+						<Link href='/' className={styles.notfound__btn} aria-label='Powrót na stronę główną'>
+							Powrót
+						</Link>
 					</div>
-				</section>
-			</main>
-			<Footer />
-		</>
+				</div>
+			</section>
+		</main>
 	);
 }
