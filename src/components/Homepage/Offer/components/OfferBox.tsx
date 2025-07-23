69e89e0666889fe94ebc82d5d3b75175bb3a9cef
Konrad <konrad.wojtylo.9@gmail.com>
2025-07-23 02:10:00 +0200

    Added special bg image to offer box

--- src/components/Homepage/Offer/components/OfferBox.tsx
+++ src/components/Homepage/Offer/components/OfferBox.tsx
@@ -2,12 +2,10 @@ import Link from 'next/link';
 import { scrollToTop } from '@/utils/scrollToTopUtils';
 import { OfferBoxModel } from '@/models/offer.model';
 
-import { PiFlowerFill } from 'react-icons/pi';
-import { BsCarFrontFill } from 'react-icons/bs';
-
 import styles from '../styles/styles.module.scss';
 
 export default function OfferBox({ id, subpage, text, title, offerPlusesArray }: OfferBoxModel) {
+	console.log(id);
 	return (
 		<div className={`${styles.offer__box} ${styles[`offer__box--${id}`]}`}>
 			<h3 className={styles['offer__box-title']}>{title}</h3>
@@ -21,13 +19,8 @@ export default function OfferBox({ id, subpage, text, title, offerPlusesArray }:
 				))}
 			</ul>
 			<Link href={subpage} className={styles['offer__box-button']} onClick={scrollToTop}>
-				Poproszę!
+				<p>Poproszę!</p>
 			</Link>
-			{title === 'Wynajem' ? (
-				<BsCarFrontFill fontSize={42} style={{ marginTop: '10px' }} />
-			) : (
-				<PiFlowerFill fontSize={42} style={{ marginTop: '10px' }} />
-			)}
 		</div>
 	);
 }
