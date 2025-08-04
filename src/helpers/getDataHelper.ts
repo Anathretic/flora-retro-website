01d5d772c1025001e026b69d1070b9d466b0464a
Konrad <konrad.wojtylo.9@gmail.com>
2025-08-04 23:44:54 +0200

    Improved data helper

--- src/helpers/getDataHelper.ts
+++ src/helpers/getDataHelper.ts
@@ -4,9 +4,11 @@ import { OpinionsDataModel } from '@/models/opinions.model';
 import { ArticleDataModel, ArticleItemModel } from '@/models/article.model';
 import { SlideDataBoxModel } from '@/models/carousel.model';
 
+const URL = 'https://flora-retro.pl';
+
 export async function getOfferData(): Promise<OfferDataModel[] | null> {
 	try {
-		const res = await fetch('https://flora-retro.pl/api/offer');
+		const res = await fetch(`${URL}/api/offer`);
 		if (!res.ok) return null;
 		const data = await res.json();
 		return data;
@@ -18,7 +20,7 @@ export async function getOfferData(): Promise<OfferDataModel[] | null> {
 
 export async function getOpinionsData(): Promise<OpinionsDataModel[] | null> {
 	try {
-		const res = await fetch('https://flora-retro.pl/api/opinions');
+		const res = await fetch(`${URL}/api/opinions`);
 		if (!res.ok) return null;
 		const data = await res.json();
 		return data;
@@ -30,7 +32,7 @@ export async function getOpinionsData(): Promise<OpinionsDataModel[] | null> {
 
 export async function getBlogArticlesData(): Promise<SlideDataBoxModel[] | null> {
 	try {
-		const res = await fetch('https://flora-retro.pl/api/articles');
+		const res = await fetch(`${URL}/api/articles`);
 		if (!res.ok) return null;
 		const data = await res.json();
 		return data;
@@ -42,7 +44,7 @@ export async function getBlogArticlesData(): Promise<SlideDataBoxModel[] | null>
 
 export async function getArticlesData(): Promise<ArticleItemModel[] | null> {
 	try {
-		const res = await fetch('https://flora-retro.pl/api/articles');
+		const res = await fetch(`${URL}/api/articles`);
 		if (!res.ok) return null;
 		const data = await res.json();
 		return data;
@@ -54,7 +56,7 @@ export async function getArticlesData(): Promise<ArticleItemModel[] | null> {
 
 export async function getRentCarImagesData(): Promise<SlideDataBoxModel[] | null> {
 	try {
-		const res = await fetch('https://flora-retro.pl/api/images');
+		const res = await fetch(`${URL}/api/images`);
 		if (!res.ok) return null;
 		const data = await res.json();
 		return data;
@@ -66,7 +68,7 @@ export async function getRentCarImagesData(): Promise<SlideDataBoxModel[] | null
 
 export async function getArticleById(id: string): Promise<ArticleDataModel | null> {
 	try {
-		const res = await fetch(`https://flora-retro.pl/api/articles/${id}`, {
+		const res = await fetch(`${URL}/api/articles/${id}`, {
 			next: { revalidate: 60 },
 		});
 		if (!res.ok) return notFound();
